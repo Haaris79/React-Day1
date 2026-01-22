04229270f773ec35bf67f7986d3f42bc8e082f81
Haaris-Dev <harisabuhanifa790@gmail.com>
2026-01-22 13:39:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css' //optional to use in css page
+import './App.css'
+import Day1 from './Day1';
+
+
+//Browser Render
+function App() {  //its is a functionak component to react
+  return ( //its is a retrun statement from react
+    <>
+
+  <Day1/>    
+    </>
+  );
+}
+
+export default App   //its is export type
---
+++ src/Day1.jsx
@@ -0,0 +1,40 @@
+//what is react?
+    //React js is a javascript library for building frontend application or user interface (UI)4
+    //React js allows us to create reusable UI components.
+    //Component are the building blocks of any react app
+    //it is created by facebook
+
+//Advantage of React js?
+    //reusable component
+    //open source
+    //fast and efficient
+    //work in browser
+    //larger comunity
+
+//prerequite knowledge to learn  React js?
+    //HTML
+    //css
+    //Tailwind or Bootstrap(optional):
+    //Javascript(ES6);
+    //NPM
+
+//what is jsx?
+    //jsx stands for javascript xml
+    //jsx is a javascript syntax  extension that allows you to write in html in react
+    //jsx makes it easier to write and add html in react
+    //jsx allows you to write in html element in javascript and place them in the dom without any createelement();
+    //and/or append() method:
+    //babel in use react for convert jsx to actual javascript
+
+
+
+export const Day1 = () => {
+    const day1="first day of react classes"
+  return ( 
+    <>
+    <h1>{day1}</h1>
+    </>
+  )
+}
+export default Day1
+
